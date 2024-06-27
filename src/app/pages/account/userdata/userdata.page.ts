c3326be6351a3e40204b31863b815471fa223cb0
Lucas Kuniyoshi <lucaskuniyoshi2002@gmail.com>
2024-06-27 11:25:26 -0300

    userImage

--- src/app/pages/account/userdata/userdata.page.ts
+++ src/app/pages/account/userdata/userdata.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import User from 'src/app/model/entities/User';
+import { AuthService } from 'src/app/model/services/auth.service';
+import { UserfirebaseService } from 'src/app/model/services/userfirebase.service';
 
 @Component({
   selector: 'app-userdata',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./userdata.page.scss'],
 })
 export class UserdataPage implements OnInit {
+  user: User | undefined;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private auth: AuthService,
+    private userService: UserfirebaseService
+  ) {}
 
   ngOnInit() {
+    this.auth.getUserLogged().subscribe(user => {
+      if (user && user.uid) {
+        this.userService.getUser(user.uid).subscribe((userData: User | undefined) => {
+          this.user = userData;
+          console.log('User loaded:', this.user);
+        });
+      } else {
+        console.error('User UID is undefined');
+      }
+    });
   }
 
 }
